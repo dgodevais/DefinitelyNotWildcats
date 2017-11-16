76e0a225fadd9c4865d2d6e92f80622efcb1dda4
colinandrus <andrus.colin@gmail.com>
2017-11-16 18:40:21 -0500

    Update get_last_review.py
    New script to get write first, last reviews to file without using too much memory.

--- get_last_review.py
+++ get_last_review.py
@@ -1,46 +1,26 @@
 import pandas as pd
-import csv
+import numpy as np
 import json
-import sys
 import datetime as dt
+import csv
 
-# run on split business.json
-biz_file = sys.argv[1]
-
-
-def recent_review(biz_id, rev_df):
-    date_list = []
-    # loop to get dates of reviews for matching biz_id
-    for i in range(len(rev_df)):
-        if rev_df['business_id'][i] == biz_id:
-            date_list.append(dt.datetime.strptime(rev_df['date'][i], '%Y-%m-%d').date())
-    try:
-        recent = max(date_list)
-    except:
-        recent = 'no review'
-    return recent
-
-
-# read in full business data
-biz = []
-with open(biz_file) as f:
-    for line in f:
-        biz.append(json.loads(line))
-
-print('restaurant.json loaded')
-
-# get all biz_ids
-id_list = [i['business_id'] for i in biz]
-
-# Make rev data a dataframe
+# only read in the review.json
+# hold on to first and last dates
+rev_dict = {}
 with open('review.json') as f:
-    rev_df = pd.DataFrame(json.loads(line) for line in f)
-
-print('reviews.json loaded')
-
-# for loop to write out biz_id + last review data
-with open(str(biz_file + 'last_reviews.csv'), 'w') as csvfile:
+    for line in f:
+        cur_rev = json.loads(line)
+        biz_id = cur_rev['business_id']
+        cur_rev_date = dt.datetime.strptime(cur_rev['date'], '%Y-%m-%d').date()
+        if biz_id in rev_dict:
+            if rev_dict[biz_id][0] > cur_rev_date:
+                rev_dict[biz_id][0] = cur_rev_date
+            elif rev_dict[biz_id][1] < cur_rev_date:
+                rev_dict[biz_id][1] = cur_rev_date
+        else:
+            rev_dict[biz_id] = [cur_rev_date, cur_rev_date]
+            
+with open(str('last_reviews.csv'), 'w') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
-    for biz_id in id_list:
-        last_rev = recent_review(biz_id, rev_df)
-        writer.writerow([biz_id] + [last_rev])
+    for key in rev_dict:
+        writer.writerow([str(key), str(rev_dict[key][0]), str(rev_dict[key][1])])  
